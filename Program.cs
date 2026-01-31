d5331ffa859b3a915e2fd52ad6fdf547b6725483
Joshua Ashby <ashby10@byu.edu>
2026-01-30 19:15:41 -0700

    finsined game

--- Program.cs
+++ Program.cs
@@ -1,85 +1,86 @@
 ﻿using System.Transactions;
+using Mission4Assignment;
 
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
-string[] gameBoardArray = ["1","2","3","4","5","6","7","8","9"];
-int i = 0;
-string board = $" {gameBoardArray[i]} | {gameBoardArray[i + 1]} | {gameBoardArray[i + 2]}\n" +
-               $"---+---+---\n" +
-               $" {gameBoardArray[i + 3]} | {gameBoardArray[i + 4]} | {gameBoardArray[i + 5]}\n" +
-               $"---+---+---\n" +
-               $" {gameBoardArray[i + 6]} | {gameBoardArray[i + 7]} | {gameBoardArray[i + 8]}";
 
-Console.WriteLine(board);
+string[] gameBoardArray = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+Support.PrintBoard(gameBoardArray);
+
 bool gameWin = false;
-while (gameWin == false)
-{
-    Console.WriteLine("Player X- Enter the number of the spot you want to choose: ");
-    string? playerXChoice = Console.ReadLine();
 
+while (!gameWin)
+{
+    int playerXChoiceInt;
     while (true)
     {
-        if (string.IsNullOrEmpty(playerXChoice))
+        Console.WriteLine("Player X - Enter a number (1-9): ");
+        string? input = Console.ReadLine();
+
+        if (!int.TryParse(input, out playerXChoiceInt) || playerXChoiceInt < 1 || playerXChoiceInt > 9)
         {
-            Console.WriteLine("Please enter a valid number between 1 and 9");
-            playerXChoice = Console.ReadLine();
+            Console.WriteLine("Invalid input.");
             continue;
         }
 
-        if (!int.TryParse(playerXChoice, out int playerXChoiceInt))
+        playerXChoiceInt--;
+
+        if (gameBoardArray[playerXChoiceInt] == "X" || gameBoardArray[playerXChoiceInt] == "O")
         {
-            Console.WriteLine("Please enter a valid number between 1 and 9");
-            playerXChoice = Console.ReadLine();
+            Console.WriteLine("That spot is already taken.");
             continue;
         }
 
-        if (playerXChoiceInt < 1 || playerXChoiceInt > 9)
-        {
-            Console.WriteLine("Please enter a valid number between 1 and 9");
-            playerXChoice = Console.ReadLine();
-            continue;
-        }
-
-        playerXChoiceInt = playerXChoiceInt - 1;
-
-        Console.WriteLine($"You chose spot {playerXChoiceInt}");
         break;
     }
-    // Put in the board update method
-    // add game winning check method
 
+    gameBoardArray[playerXChoiceInt] = "X";
+    Support.PrintBoard(gameBoardArray);
 
-    Console.WriteLine("Player O- Enter the number of the spot you want to choose: ");
-    string? playerOChoice = Console.ReadLine();
+    string result = Support.CheckWinner(gameBoardArray);
+    if (result != "")
+    {
+        EndGame(result);
+        break;
+    }
 
+    int playerOChoiceInt;
     while (true)
     {
-        if (string.IsNullOrEmpty(playerOChoice))
+        Console.WriteLine("Player O - Enter a number (1-9): ");
+        string? input = Console.ReadLine();
+
+        if (!int.TryParse(input, out playerOChoiceInt) || playerOChoiceInt < 1 || playerOChoiceInt > 9)
         {
-            Console.WriteLine("Please enter a valid number between 1 and 9");
-            playerOChoice = Console.ReadLine();
+            Console.WriteLine("Invalid input.");
             continue;
         }
 
-        if (!int.TryParse(playerOChoice, out int playerOChoiceInt))
+        playerOChoiceInt--;
+
+        if (gameBoardArray[playerOChoiceInt] == "X" || gameBoardArray[playerOChoiceInt] == "O")
         {
-            Console.WriteLine("Please enter a valid number between 1 and 9");
-            playerOChoice = Console.ReadLine();
+            Console.WriteLine("That spot is already taken.");
             continue;
         }
 
-        if (playerOChoiceInt < 1 || playerOChoiceInt > 9)
-        {
-            Console.WriteLine("Please enter a valid number between 1 and 9");
-            playerOChoice = Console.ReadLine();
-            continue;
-        }
-
-        playerOChoiceInt = playerOChoiceInt - 1;
-
-        Console.WriteLine($"You chose spot {playerOChoiceInt}");
         break;
-    }      
-    
-    // Put in the board update method
-    // add game winning check method
+    }
+
+    gameBoardArray[playerOChoiceInt] = "O";
+    Support.PrintBoard(gameBoardArray);
+
+    result = Support.CheckWinner(gameBoardArray);
+    if (result != "")
+    {
+        EndGame(result);
+        break;
+    }
+}
+
+static void EndGame(string result)
+{
+    if (result == "Tie")
+        Console.WriteLine("It's a tie!");
+    else
+        Console.WriteLine($"Player {result} wins!");
 }
